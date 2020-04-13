191efe26cb2ed1a801a8f791bfcbddfbb75e9d5e
guitarmike1966 <guitarmike1966@gmail.com>
2020-04-13 12:31:25 -0500

    Initial Commit

---
+++ CalcTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcTest
+//
+//  Created by mikey on 4/13/20.
+//  Copyright © 2020 Sand Creek Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
